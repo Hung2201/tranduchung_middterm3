547e65581cb4cfe27231fb9146d5801d0ddb9094
Hung2201 <duchung12a8@gmail.com>
2024-06-05 17:02:26 +0700

    Create About and NotFound components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Search from "./components/users/Search";
+import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
+
 const App = () => {
   return (
     <div className="App">
-      <Navbar />
-      <div className="container">
-        <h1>GitHub Users Data</h1>
-        <Search />
-      </div>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Search />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 };
+
 export default App;
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,23 +1,22 @@
-// /src/components/users/UserItem.js
 import React from "react";
+import { Link } from "react-router-dom";
 const UserItem = (props) => {
-  const { login, avatar_url } = props.user;
-  return (
-    <div className="card text-center">
-      <img
-        src={avatar_url}
-        alt=""
-        className="round-img"
-        style={{ width: "60px" }}
-      />
-      <h3>{login}</h3>
-      <div>
-        <a href="#" className="btn btn-dark btn-sm my-1">
-          More
-        </a>
-      </div>
-      
-    </div>
-  );
+    const { login, avatar_url} = props.user;
+    return (
+        <div className="card text-center">
+            <img
+                src={avatar_url}
+                alt=""
+                className="round-img"
+                style={{ width: "60px" }}
+            />
+            <h3>{login}</h3>
+            <div>
+                <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
+                    More
+                </Link>
+            </div>
+        </div>
+    );
 };
 export default UserItem;
